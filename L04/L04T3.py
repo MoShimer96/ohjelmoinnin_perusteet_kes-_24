5c957267e180e1c00b057b4d62610e29c7afec89
MoShimer96 <9moal6@gmail.com>
2024-06-12 07:11:38 +0300

    Made final changes

--- L04/L04T3.py
+++ L04/L04T3.py
@@ -4,27 +4,29 @@ def paaohjelma():
     setelienLukumaara = 0
     setelienArvo = 0
     setelienKokonaisArvo = 0
-    print("Tällä ohjelmalla voit analysoida eurosetelien tietoja.", end="")
+    print("Tällä ohjelmalla voit analysoida eurosetelien tietoja.\n", end="")
     
     while True:
-        toiminto = int(input("Valitse haluamasi toiminto:\n1)Kysy setelin arvo\n2)Kysy setelien lukumäärä\n3)Laske kokonaisarvo\n4)Tulosta tulos\n0)Lopeta\nAnna valintasi: "))
+        toiminto = int(input("Valitse haluamasi toiminto:\n1) Kysy setelin arvo\n2) Kysy setelien lukumäärä\n3) Laske kokonaisarvo\n4) Tulosta tulos\n0) Lopeta\nAnna valintasi: "))
         if toiminto == 1:
-            setelienArvo = int(input("anna analysoitavan setelin arvo: "))
+            setelienArvo = int(input("Anna analysoitavan setelin arvo: "))
+            print("")
             continue
         elif toiminto == 2:
             setelienLukumaara = int(input("Anna setelien lukumäärä: "))
+            print("")
             continue
         elif toiminto == 3:
             setelienKokonaisArvo = setelienLukumaara * setelienArvo
-            print("Setelien kokonaisarvo laskettu.", end="\n")
+            print("Setelien kokonaisarvo laskettu.\n", end="\n")
             continue
         elif toiminto == 4:
-            print(f"Setelien kokonaisarvo on {setelienKokonaisArvo} euroa.", end="\n")
+            print(f"Setelien kokonaisarvo on {setelienKokonaisArvo} euroa.\n", end="\n")
             continue
         elif toiminto == 0:
             print("Lopetetaan.\n", end="\n")
             break
-    print("Kiitos Ohjelman käytöstä.", end="\n")
+    print("Kiitos ohjelman käytöstä.", end="\n")
     
     return None
 
