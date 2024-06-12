084c38d775d5da496330ac620485d7e04f2f224c
MoShimer96 <9moal6@gmail.com>
2024-06-12 06:57:51 +0300

    Added changes to the print so that it passes the tests

--- L04/L04T3.py
+++ L04/L04T3.py
@@ -4,29 +4,29 @@ def paaohjelma():
     setelienLukumaara = 0
     setelienArvo = 0
     setelienKokonaisArvo = 0
-    print("Tällä ohjelmalla voit analysoida eurosetelien tietoja.")
+    print("Tällä ohjelmalla voit analysoida eurosetelien tietoja.", end=" ")
     
     while True:
         toiminto = int(input("Valitse haluamasi toiminto:\n1)Kysy setelin arvo\n2)Kysy setelien lukumäärä\n3)Laske kokonaisarvo\n4)Tulosta tulos\n0)Lopeta\nAnna valintasi: "))
         if toiminto == 1:
             setelienArvo = int(input("anna analysoitavan setelin arvo: "))
-            print("\n")
+            print(end="\n")
             continue
         elif toiminto == 2:
             setelienLukumaara = int(input("Anna setelien lukumäärä: "))
-            print("\n")
+            print(end="\n")
             continue
         elif toiminto == 3:
             setelienKokonaisArvo = setelienLukumaara * setelienArvo
-            print("Setelien kokonaisarvo laskettu.\n")
+            print("Setelien kokonaisarvo laskettu.\n", end=" ")
             continue
         elif toiminto == 4:
-            print(f"Setelien kokonaisarvo on {setelienKokonaisArvo} euroa\n")
+            print(f"Setelien kokonaisarvo on {setelienKokonaisArvo} euroa\n", end=" ")
             continue
         elif toiminto == 0:
-            print("Lopetetaan.\n")
+            print("Lopetetaan.\n", end=" ")
             break
-    print("Kiitos Ohjelman käytöstä.")
+    print("Kiitos Ohjelman käytöstä.", end=" ")
     
     return None
 
